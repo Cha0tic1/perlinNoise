e2c25f818ad1e248b4c146a83b7686f9d160955e
Nissen <thomas.g.nissen@gmail.com>
2025-02-04 09:45:35 +0100

    Noise implenteret, manglende visualisering.

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ class Vector2D
         this.y = y;
     }
 
+    // Funktion for dotprodukt af vores vektorer.
     dot(konstantVektor)
     {
         return this.x*konstantVektor.x + this.y*konstantVektor.y;
@@ -16,26 +17,46 @@ class Vector2D
 
 function setup() 
 {
-	createCanvas(400, 400);
+	createCanvas(100, 100);
+    loadPixels();
+    let d = pixelDensity();
+    let w = d * width;
+    let h = d * height;
+
+    for(let x = 0; x<w; x++)
+    {
+        for(let y = 0; y<h; y++)
+        {
+            let index = 4*(x+y*w);
+            //let n = noise2D(x,y);
+            //let brightness = map(n,-1,1,0,255)
+
+            pixels[index]=0;
+            pixels[index+1]=0;
+            pixels[index+2]=0;
+            pixels[index+3]=0;
+        }
+    }
+       
 }
 
 function draw()
 {
-    
+    updatePixels()
 }
 
 function permutationsTabel()
 {
     // Lav en tabel fra 0-255
-    let P = [];
+    let pTabel = [];
     for(let i = 0; i<256; i++)
     {
-        P[i]=i;
+        pTabel[i]=i;
     }
-    // Bland tabellen tilfældigt
-    return shuffle(P); 
+    // Bland tabellen tilfældigt vha p5 indbygget shuffle funktion
+    return shuffle(pTabel); 
 }
-let P = permutationsTabel()
+let pTabel = permutationsTabel()
 
 function lavVektor(ptable)
 {
@@ -60,13 +81,30 @@ function lavVektor(ptable)
     }
 }
 
-function noise2D()
+// En funktion til at interpolere to værdier med hinanden.
+function interpol(t,v1,v2)
+{
+    return v1+t*(v2-v1);
+} 
+
+// Vi laver en smooth overgang mellem vores værdier, således at vi kan få
+// smooth overgange i stedet for bratte skift i vores værdier, og noget der
+// minder om tv-støj.
+function trans(t)
+{
+    return 6*t*t*t*t*t - 15*t*t*t*t + 10*t*t*t
+}
+
+function noise2D(x,y)
 {
     // Vi bruger bitwise AND til at sikre at vores værdi ikke overstiger
     // 256, da det indeks ikke eksisterer i vores permutationsTabel
     const X = Math.floor(x) & 255;
     const Y = Math.floor(y) & 255;
 
+    // Vi minuser x med floor(x) sådan så vi får et decimal tal, afhængigt af
+    // x og y koordinatet de skal tegnes til, således at alle vektorer, har
+    // samme længde.
     const xf = x-Math.floor(x);
     const yf = y-Math.floor(y);
 
@@ -77,14 +115,21 @@ function noise2D()
     const bundVenstre = new Vector2D(xf,yf);
 
     // Find en værdi for hvert hjørne, afhængigt af vores permutationsTabel
-    const værdiTopHøjre = P[P[X+1]+Y+1]
-    const værdiTopVenstre = P[P[X]+Y+1]
-    const værdiBundHøjre = P[P[X+1]+Y]
-    const værdiBundVenstre = P[P[X]+Y]
+    const værdiTopHøjre = pTabel[pTabel[X+1]+Y+1]
+    const værdiTopVenstre = pTabel[pTabel[X]+Y+1]
+    const værdiBundHøjre = pTabel[pTabel[X+1]+Y]
+    const værdiBundVenstre = pTabel[pTabel[X]+Y]
 
     // Tag dot produktet af vores konstante vektorer og vores tilfældige.
     const dotTopHøjre = topHøjre.dot(lavVektor(værdiTopHøjre));
     const dotTopVenstre = topVenstre.dot(lavVektor(værdiTopVenstre));
     const dotBundHøjre = bundHøjre.dot(lavVektor(værdiBundHøjre));
     const dotBundVenstre = bundVenstre.dot(lavVektor(værdiBundVenstre));
+
+    // Lav 2 værdier som bliver vores t værdi i interpol funktionen.
+    // u er t værdien for interpolation langs x-aksen
+    // v er t værdien for interpolation langs y-aksen
+    let u = trans(xf)
+    let v = trans(yf)
+    return interpol(u,interpol(v,dotTopHøjre,dotBundHøjre),interpol(v,dotTopVenstre,dotBundVenstre))
 }
